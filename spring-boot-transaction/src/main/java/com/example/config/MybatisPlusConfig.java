7922430a2d95cc47c13a327f578eefe03af12891
Even <286937628@qq.com>
2018-12-14 16:29:22 +0800

    spring boot 事物

---
+++ spring-boot-transaction/src/main/java/com/example/config/MybatisPlusConfig.java
@@ -0,0 +1,57 @@
+package com.example.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
+import com.baomidou.mybatisplus.core.parser.ISqlParser;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PerformanceInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Administrator on 2018/12/10 0010.
+ *
+ * @author even
+ */
+@Configuration
+@MapperScan("com.example.**.mapper")
+public class MybatisPlusConfig {
+    /**
+     * mybatis-plus分页插件<br>
+     * 文档：http://mp.baomidou.com<br>
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        /*
+         * 【测试多租户】 SQL 解析处理拦截器<br>
+         * 这里固定写成住户 1 实际情况你可以从cookie读取，因此数据看不到 【 麻花藤 】 这条记录（ 注意观察 SQL ）<br>
+         */
+        List<ISqlParser> sqlParserList = new ArrayList<>();
+        paginationInterceptor.setSqlParserList(sqlParserList);
+        return paginationInterceptor;
+    }
+
+    @Bean
+    @ConfigurationProperties("spring.datasource.druid")
+    public DruidDataSource dataSource() {
+        return DruidDataSourceBuilder
+                .create()
+                .build();
+    }
+
+
+
+    /**
+     * 性能分析拦截器，不建议生产使用
+     */
+    @Bean
+    public PerformanceInterceptor performanceInterceptor(){
+        return new PerformanceInterceptor();
+    }
+}
