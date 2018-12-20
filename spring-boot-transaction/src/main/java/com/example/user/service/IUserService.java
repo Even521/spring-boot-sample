fd55d3337bd2dcdc4202a40abd3e216ad70f5f52
Even <286937628@qq.com>
2018-12-20 18:03:09 +0800

    去掉没有用的包

--- spring-boot-transaction/src/main/java/com/example/user/service/IUserService.java
+++ spring-boot-transaction/src/main/java/com/example/user/service/IUserService.java
@@ -2,6 +2,10 @@ package com.example.user.service;
 
 import com.example.user.entity.User;
 import com.baomidou.mybatisplus.extension.service.IService;
+import org.springframework.ui.Model;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.xml.ws.Response;
 
 /**
  * <p>
@@ -12,6 +16,8 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2018-12-14
  */
 public interface IUserService extends IService<User> {
+
+
     /**
      * 保存一个用户
      * @return
