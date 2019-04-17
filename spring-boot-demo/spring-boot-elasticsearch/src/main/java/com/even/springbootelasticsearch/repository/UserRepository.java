3b4cbe391967bdbd4418b725ada1f86ff52bb472
even <even366>
2019-04-17 11:31:11 +0800

    es demo

--- spring-boot-demo/spring-boot-elasticsearch/src/main/java/com/even/springbootelasticsearch/repository/UserRepository.java
+++ spring-boot-demo/spring-boot-elasticsearch/src/main/java/com/even/springbootelasticsearch/repository/UserRepository.java
@@ -2,14 +2,11 @@ package com.even.springbootelasticsearch.repository;
 
 import com.even.springbootelasticsearch.domain.User;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-import org.springframework.stereotype.Component;
-
 /**
  * @author even
  * @date 2019/4/16 15:53
  * @describe
  */
-@Component
 public interface UserRepository extends ElasticsearchRepository<User,String> {
 
 
