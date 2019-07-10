2309148ef79494e796eb31c17c529377f3396128
even <even366@qq.com>
2019-07-10 13:50:06 +0800

    spring-boot-quartz

---
+++ spring-boot-demo/spring-boot-quartz/src/main/java/com/even/SpringBootQuartzApplication.java
@@ -0,0 +1,16 @@
+package com.even;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+
+
+@SpringBootApplication
+public class SpringBootQuartzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootQuartzApplication.class, args);
+    }
+
+}
