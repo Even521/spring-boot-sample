3a3c7d820da022d521b14811de682a653a25c031
even <even366@qq.com>
2019-07-16 14:47:30 +0800

    spring-boot-quartz

---
+++ spring-boot-demo/spring-boot-hikari/src/main/java/com/even/springboothikari/SpringBootHikariApplication.java
@@ -0,0 +1,13 @@
+package com.even.springboothikari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHikariApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHikariApplication.class, args);
+    }
+
+}
