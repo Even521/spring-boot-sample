8df701344165619afd64690c5de821c664a1fec1
even <even366@qq.com>
2019-09-17 09:45:36 +0800

    spring boot 配置文件敏感信息加密

---
+++ spring-boot-demo/spring-boot-config-file-encryption/src/main/java/com/even/SpringBootConfigFileEncryptionApplication.java
@@ -0,0 +1,13 @@
+package com.even;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConfigFileEncryptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootConfigFileEncryptionApplication.class, args);
+    }
+
+}
