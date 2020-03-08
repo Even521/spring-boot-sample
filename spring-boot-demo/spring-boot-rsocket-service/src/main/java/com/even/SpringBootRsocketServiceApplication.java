3c60f6ef7f83f9560965ce9551e6166b4720c253
even <286937628@qq.com>
2020-03-08 21:20:03 +0800

    Rsocket demo

---
+++ spring-boot-demo/spring-boot-rsocket-service/src/main/java/com/even/SpringBootRsocketServiceApplication.java
@@ -0,0 +1,13 @@
+package com.even;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRsocketServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRsocketServiceApplication.class, args);
+    }
+
+}
